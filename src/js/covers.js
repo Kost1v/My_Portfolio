2da2e6f507505106ba0a056926be7b6d93631e3f
Ostap <ostapcostiv@gmail.com>
2025-09-18 18:38:50 +0300

    portfolio

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,35 +1,35 @@
-const coversSection = document.querySelector('.covers');
-const marqueeInner = document.querySelectorAll('.marquee__line');
+// const coversSection = document.querySelector('.covers');
+// const marqueeInner = document.querySelectorAll('.marquee__line');
 
-function startAnimation() {
-  marqueeInner.forEach(item => {
-    item.style.animationPlayState = 'running'; 
-  });
-}
+// function startAnimation() {
+//   marqueeInner.forEach(item => {
+//     item.style.animationPlayState = 'running';
+//   });
+// }
 
-function stopAnimation() {
-  marqueeInner.forEach(item => {
-    item.style.animationPlayState = 'paused'; 
-  });
-}
-
-
- const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      startAnimation(); 
-    } else {
-      stopAnimation(); 
-      }
-    
-      });
-}, {
-    threshold: 0.1,
-});
+// function stopAnimation() {
+//   marqueeInner.forEach(item => {
+//     item.style.animationPlayState = 'paused';
+//   });
+// }
 
 
-observer.observe(coversSection);
+//  const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       startAnimation();
+//     } else {
+//       stopAnimation();
+//       }
+
+//       });
+// }, {
+//     threshold: 0.1,
+// });
 
 
-stopAnimation();
+// observer.observe(coversSection);
+
+
+// stopAnimation();
 
